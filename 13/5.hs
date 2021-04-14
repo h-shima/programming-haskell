ff197c45d09a006e73b9ff59f497998fe4544f54
Oshima Hiroki <hiroki.oshima@MOB20007.local>
2021-04-14 11:45:33 +0900

    Impl 5

---
+++ 13/5.hs
@@ -0,0 +1,11 @@
+-- (BNF) expr ::= term(+expr | )
+
+data Expr = Val Int | Add Expr Expr deriving Show
+
+expr' :: Parser Expr
+expr' = do t <- term
+           do symbol "+"
+              e <- expr'
+              return (Add (Val t) e)
+           <|> return (Val t)
+
