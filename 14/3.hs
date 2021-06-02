f2d3dc8b15fca33f636c2c24b1f9db649d071965
Oshima Hiroki <hiroki.oshima@MOB20007.local>
2021-06-02 11:40:42 +0900

    デフォルト実装を使っていた箇所を実装

--- 14/3.hs
+++ 14/3.hs
@@ -1,19 +1,40 @@
-instance Monoid a => Monoid (Maybe a) where
+import Data.Foldable
+
+data Maybe' a = Just' a | Nothing' deriving Show
+
+instance Monoid a => Monoid (Maybe' a) where
   -- mempty :: a
-  mempty = Nothing
+  mempty = Nothing'
 
   -- mappend ::
-  Nothing `mappend` m = m
-  m `mappend` Nothing = m
-  Just m1 `mappend` Just m2 = Just (m1 `mappend` m2)
+  Nothing' `mappend` m = m
+  m `mappend` Nothing' = m
+  Just' m1 `mappend` Just' m2 = Just' (m1 `mappend` m2)
 
-instance Foldable Maybe where
-  -- foldMap :: Monoid b => (a -> b) -> Maybe a -> b
-  foldMap _ Nothing = mempty
-  foldMap f (Just x) = f x
+instance Foldable Maybe' where
+  -- foldMap :: Monoid b => (a -> b) -> Maybe' a -> b
+  foldMap _ Nothing' = mempty
+  foldMap f (Just' x) = f x
 
-instance Traversable Maybe where
+  -- fold :: Monoid a => Maybe' a -> a
+  fold Nothing' = mempty
+  fold (Just' x) = x
+
+  -- foldr :: (a -> b -> b) -> b -> [a] -> b
+  -- foldl :: (a -> b -> a) -> a -> [b] -> a
+  -- わからず...
+
+instance Functor Maybe' where
+  fmap f Nothing' = Nothing'
+  fmap f (Just' x) = Just' (f x)
+
+instance Applicative Maybe' where
+  pure = Just'
+  Nothing' <*> _ = Nothing'
+  (Just' f) <*> something = fmap f something
+
+instance Traversable Maybe' where
   -- traverse :: Applicative f => (a -> f b) -> Maybe a -> f (Maybe b)
-  traverse _ Nothing = pure Nothing
-  traverse g (Just x) = fmap Just (g x)
+  traverse _ Nothing' = pure Nothing'
+  traverse g (Just' x) = fmap Just' (g x)
 
