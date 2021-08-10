ed28c7c5f1b7a077c56299aa7583039de8d41a91
Hiroki Oshima <geet1876@gmail.com>
2021-08-11 07:16:56 +0900

    WIP 17-1

--- 17/language_with_exception.hs
+++ 17/language_with_exception.hs
@@ -1,7 +1,8 @@
--- 例外を扱える評価器
+-- 文法
 data Expr = Val Int | Add Expr Expr | Throw | Catch Expr Expr
 
-eval :: Expr -> Maybe int
+-- 意味
+eval :: Expr -> Maybe Int
 eval (Val n)   = Just n
 eval (Add x y) = case eval x of
                    Nothing -> Nothing
@@ -13,7 +14,37 @@ eval (Catch x h) = case eval x of
                      Nothing -> eval h
                      Just n  -> Just n
 
--- この言語のコンパイラの算出
--- この文献の最後の方に答えが載ってる。。。
+-- ターゲット言語の種類
+data Code = HALT
+          | PUSH Int Code
+          | ADD Code
+          | MARK Code
+          | UNMARK Code
+          | FAIL
+          deriving (Show)
+
+-- コンパイラ(目標言語であるCodeまで)
+comp :: Expr -> Code
+comp e = comp' e HALT
+
+comp' :: Expr -> Code -> Code
+comp' (Val n) c = PUSH n c
+comp' (Add x y) c = comp' x (comp' y (ADD c))
+comp' Throw c = FAIL
+comp' (Catch x h) c = MARK (comp' h c) (comp' x (UNMARK c))
+
+-- Stack
+type Stack = [Int]
+
+-- Virtual Machine
+exec :: Code -> Stack -> Stack
+exec HALT s = s
+exec (PUSH n c) s = exec c (n : s)
+exec (MARK h c) s = exec c (h : s)
+
+-- WIP
+-- MARK, UNMARKの扱い
+-- exec（Code -> Stack）の定義
+
 -- https://pdfs.semanticscholar.org/8ec2/9246b708ca149a4dc05d088a3b4f7396b14b.pdf
 
